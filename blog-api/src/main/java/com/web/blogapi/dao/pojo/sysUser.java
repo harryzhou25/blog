61f18c55074ecd3181d80059baf5796a3d6141b5
Harry <harryzhou25@outlook.com>
2024-06-07 01:42:50 +1000

    Add user entity.

---
+++ blog-api/src/main/java/com/web/blogapi/dao/pojo/sysUser.java
@@ -0,0 +1,32 @@
+package com.web.blogapi.dao.pojo;
+
+import lombok.Data;
+
+@Data
+public class sysUser {
+    private Long id;
+
+    private String account;
+
+    private Integer admin;
+
+    private String avatar;
+
+    private Long createDate;
+
+    private Integer deleted;
+
+    private String email;
+
+    private Long lastLogin;
+
+    private String mobilePhoneNumber;
+
+    private String nickname;
+
+    private String password;
+
+    private String salt;
+
+    private String status;
+}
