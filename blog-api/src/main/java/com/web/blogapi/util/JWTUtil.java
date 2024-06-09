cf2cbec874deac6fc7fb84b4e5769dbc47b447a1
Harry <harryzhou25@outlook.com>
2024-06-09 17:36:47 +1000

    Added exception handler.

---
+++ blog-api/src/main/java/com/web/blogapi/util/JWTUtil.java
@@ -0,0 +1,36 @@
+package com.web.blogapi.util;
+
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JWTUtil {
+    private static final String jwtToken = "123456Mszlu!@#$$";
+
+    public static String createToken(Long userId){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("userId",userId);
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, jwtToken)
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 24L * 60 * 60 * 60 * 1000));
+        return jwtBuilder.compact();
+    }
+
+    public static Map<String, Object> checkToken(String token){
+        try {
+            Jwt parse = Jwts.parser().setSigningKey(jwtToken).parse(token);
+            return (Map<String, Object>) parse.getBody();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+}
